a792f3442c45648581518f13a6f71f0020d7ceb5
sabinuki <blue.apple.pie63@gmail.com>
2022-09-09 10:30:57 +0900

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by 内間悠人 on 2022/09/09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/PhotoBGFilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoBGFilterApp.swift
+//  Shared
+//
+//  Created by 内間悠人 on 2022/09/09.
+//
+
+import SwiftUI
+
+@main
+struct PhotoBGFilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
